8596a0b996e36fc639910d1044bb9071f3c46f6e
Kyle Perez <kylepereza@gmail.com>
2024-07-20 17:47:27 -0600

    j-ending adjectives, and i and u stem adjectives

--- adjectives.js
+++ adjectives.js
@@ -24,18 +24,26 @@ const weak_neuter_endings = [['-o', '--', '-ona'], ['-ins', '--', '-ane'], ['-in
 const strong_feminine_endings = [['-a', '--', '-os'], ['-aizos', '--', '-aizo'], ['-ai', '--', '-aim'], ['-a', '--', '-os'], ['--', '--', '--']];
 const weak_feminine_endings = [['-o', '--', '-ons'], ['-ons', '--', '-ono'], ['-on', '--', '-om'], ['-on', '--', '-ons'], ['--', '--', '--']];
 
-const standard = new adjectivetype('Standard', [[strong_masculine_endings, weak_masculine_endings], [strong_neuter_endings, weak_neuter_endings], [strong_feminine_endings, weak_feminine_endings]])
+const standard = new adjectivetype('Standard', [[strong_masculine_endings, weak_masculine_endings], [strong_neuter_endings, weak_neuter_endings], [strong_feminine_endings, weak_feminine_endings]]);
 
+const strong_masculine_shortj_endings = [['-is', '--', '-ai'], ['-is', '--', '-aize'], ['-amma', '--', '-aim'], ['-ana', '--', '-ans'], ['--', '--', '--']];
+const standard_short_jending = new adjectivetype('Standard (j ending, short vowel)', [[strong_masculine_shortj_endings, weak_masculine_endings], [strong_neuter_endings, weak_neuter_endings], [strong_feminine_endings, weak_feminine_endings]]);
+
+const strong_feminine_longj_endings = [['-', '--', '-os'], ['-aizos', '--', '-aizo'], ['-ai', '--', '-aim'], ['-a', '--', '-os'], ['--', '--', '--']];
+const standard_long_jending = new adjectivetype('Standard (j ending, long vowel)', [[strong_masculine_shortj_endings, weak_masculine_endings], [strong_neuter_endings, weak_neuter_endings], [strong_feminine_longj_endings, weak_feminine_endings]]);
+
+const strong_feminine_istem_endings = [['-s', '--', '-os'], ['-aizos', '--', '-aizo'], ['-ai', '--', '-aim'], ['-a', '--', '-os'], ['--', '--', '--']];
+const istem = new adjectivetype('i-stem', [[strong_masculine_endings, weak_masculine_endings], [strong_neuter_endings, weak_neuter_endings], [strong_feminine_istem_endings, weak_feminine_endings]]);
+
+// Wiktionary claims that singular genitive endings for this type are '-us' as in hardaus, with '-uus' --> 'aus'
+// But this isn't an attested form. Lambdin claims it's just like wilþi.
+// Lambdin form is simpler, so just use that!
+const ustem = new adjectivetype('i-stem', [[strong_masculine_endings, weak_masculine_endings], [strong_neuter_endings, weak_neuter_endings], [strong_feminine_istem_endings, weak_feminine_endings]]);
 
 // Getting the root of a noun
 function get_adjective_root(adjective_name, adjective_type) {
-
-	let stem = '';
-
-	stem = adjective_name.slice(0, -1);
-	tools.print('Stem: ' + stem);
+	let stem = adjective_name.slice(0, -1);
 	return stem;
-
 }
 
 
@@ -49,20 +57,44 @@ class adjective {
 		this.name = name;
 		this.root = get_adjective_root(name, adjective_type);
 		this.endings = adjective_type.endings;
+		this.is_long_jstem = this.#is_long_jstem();
+		this.is_istem = this.#is_istem();
+		this.is_ustem = this.#is_ustem();
 		this.definition = definition;
 		this.overrides = overrides;
 	}
 
+	#is_long_jstem() {
+		// First check if noun is a j stem to begin with
+		if (this.root.slice(-1) != 'j') return false;
+		// Then see if stem satisfies CvCCj patern. If so, it is a long j stem for all vowels v and constanants C
+		// To see if it is a short vowel j stem, has pattern of CvCj- where v is short for ending letters
+		let imp_char = this.root.slice(-3, -2);
+		if (tools.isConsanant(imp_char)) return true;
+		// Take advantage of the fact that we have a CvC... like pattern; complete vowel is always preceeded by a consanant
+		// In Gothic, the vowels can be represented by two characters though, so need to know this
+		else {
+			let prev_imp_char = this.root.slice(-4, -3);
+			let vowel = '';
+			if (tools.isConsanant(prev_imp_char)) {
+				vowel = imp_char;
+			}
+			else {
+				vowel = prev_imp_char + imp_char;
+			}
+
+			// Long Vowel values are 'e' 'o' and 'ei'
+			// short vowels are 'a', 'i', and 'u', but also includes all the others for now.
+			return tools.LongVowels.includes(vowel);
+		}
+	}
+
+	#is_istem() { return this.root.slice(-1) == 'i'; }
+	#is_ustem() { return this.root.slice(-1) == 'u'; }
+
 	decline(adjective_gender, adjective_declension, adjective_case, adjective_number) {
 
 		let declination = 'Unknown Declination';
-
-		let gen = adjective_gender.name;
-		let dec = adjective_declension.name;
-		let cas = adjective_case.name;
-		let num = adjective_number.name;
-
-		//tools.print(gen + dec + cas + num);
 
 		if (!Genders.GenderList.includes(adjective_gender)) {
 			declination = 'Unknown Adjective Gender "' + adjective_gender.name + '"';
@@ -86,6 +118,41 @@ class adjective {
 		// If no declination given, show just '-'
 		if (tools.isNullEnding(ending)) {
 			return ending;
+		}
+		// Special cases if declination is just noun root and that root ends with 'j'
+		else if (this.root.slice(-1) == 'j') {
+			declination = this.root + ending;
+			if (declination.slice(-1) == 'j') {
+				// Reduce j to i
+				declination = declination.slice(0, -1) + 'i';
+				// Then if previous character is a 'u' convert that to a 'w'
+				if (declination.slice(-2, -1) == 'u') {
+					declination = declination.slice(0, -2) + 'wi';
+				}
+			}
+			// Otherwise if adjective is long j stem (masculine or neuter are the ones that apply), assimilate 'jis' to eis'
+			else if (this.is_long_jstem && ending == 'is') {
+				//declination = declination.slice(0,-3) + 'eis';
+				declination = this.root.slice(0, -1) + 'eis';
+			}
+		}
+		else if (this.is_istem) {
+			declination = this.root + ending;
+			if (declination.slice(-2) == 'is') {
+				declination = this.root.slice(0, -1) + ending;
+			}
+			else if (declination == this.root) {
+				declination = declination.slice(0, -1);
+			}
+			else {
+				declination = this.root.slice(0, -1) + 'j' + ending;
+			}
+		}
+		else if (this.is_ustem) {
+			declination = this.root + ending;
+			if (declination.slice(-2) != 'us' && declination != this.root) {
+				declination = this.root.slice(0, -1) + 'j' + ending;
+			}
 		}
 		else {
 			declination = this.root + ending;
@@ -155,6 +222,23 @@ class adjective {
 
 
 /*
+// Standard Adjective
 let mikil = new adjective('mikil-', standard, NoOverrides, 'Large, big, important');
 mikil.print_cases();
+
+// Short j ending
+let niuj = new adjective('niuj-', standard_short_jending, NoOverrides, 'New');
+niuj.print_cases();
+
+// Short j ending
+let wilþj = new adjective('wilþj-', standard_long_jending, NoOverrides, 'New');
+wilþj.print_cases();
+
+// i stem ending
+let hraini = new adjective('hraini-', istem, NoOverrides, 'Pure, clean');
+hraini.print_cases();
 */
+
+// u stem ending
+let hardu = new adjective('hardu-', ustem, NoOverrides, 'Difficult, hard');
+hardu.print_cases();
